cae55172d71db8c5a620cf884ac1ed8a00191dfe
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-02-07 23:32:33 +0530

    [Ashish] Add:Data management of rice, pulses and wheats added

---
+++ InventoryManangement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManangement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InventoryManagement inventoryManagement = new  InventoryManagement();
+            inventoryManagement.ReadData(@"C:\Users\Lenovo\Desktop\git project\OOPS\InventoryManangement\Inventory.json");
+            Console.ReadKey();
+        }
+    }
+}
